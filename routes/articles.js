fefe92ff5ebb861f5e7d46d56d1438fd13df86be
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2021-09-04 22:34:17 +0300

    add updating article

--- routes/articles.js
+++ routes/articles.js
@@ -29,15 +29,35 @@ export default async (fastify) => {
     return reply;
   });
 
-  fastify.get('/articles/:id/edit', { name: 'editArticle' }, async (req, reply) => {
-    reply.render('index');
-    return reply;
-  });
-
   fastify.get('/articles/:id', { name: 'article' }, async (req, reply) => {
     reply.render('index');
     return reply;
   });
+
+  fastify.get('/articles/:id/edit', { name: 'editArticle' }, async (req, reply) => {
+    const { id } = req.params;
+    const article = await fastify.db.models.Article.findByPk(id);
+    reply.render('articles/edit', { article });
+    return reply;
+  });
+
+  fastify.patch('/articles/:id', async (req, reply) => {
+    const { id } = req.params;
+    const article = await fastify.db.models.Article.findByPk(id);
+    Object.assign(article, req.body.data);
+
+    try {
+      await article.save();
+      req.flash('info', fastify.t('views.article.edit.success'));
+      reply.redirect(fastify.reverse('articles'));
+    } catch ({ errors }) {
+      req.flash('error', fastify.t('views.article.edit.error'));
+      reply.code(422);
+      reply.render('articles/edit', { article, errors });
+    }
+
+    return reply;
+  })
 
   fastify.delete('/articles/:id', async (req, reply) => {
     const { id } = req.params;
