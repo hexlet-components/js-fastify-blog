a9066c18fff9ad873959aff845d9caf68d64c2d0
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2021-09-04 17:03:44 +0300

    fix load sequelize models & create Article

--- plugins/sequelize.js
+++ plugins/sequelize.js
@@ -3,12 +3,20 @@
 import fp from 'fastify-plugin';
 import sequelizeFastify from 'sequelize-fastify';
 import dbConfig from '../config/config.cjs';
+import db from '../models/index.cjs';
 
 export default fp(async (fastify) => {
   const mode = process.env.NODE_ENV ?? 'development';
 
-  fastify.register(sequelizeFastify, {
-    instance: 'db',
-    sequelizeOptions: dbConfig[mode],
-  });
+  fastify
+    .register(sequelizeFastify, {
+      instance: 'db',
+      sequelizeOptions: dbConfig[mode],
+    })
+    .after((err) => {
+      if (err) throw err;
+      // NOTE: sequelize do not add models to instance
+      // eslint-disable-next-line
+      fastify.db.models = db.sequelize.models;
+    });
 });
--- routes/root.js
+++ routes/root.js
@@ -2,6 +2,14 @@
 
 export default async (fastify) => {
   fastify.get('/', async () => {
+    // const article = fastify.db.models.Article.build({
+    //   name: 'some name',
+    //   content: 'lala-lala',
+    // });
+    // await article.save();
+    // console.log(article);
+    const articles = await fastify.db.models.Article.findAll();
+    console.log(articles);
     return 'Hello, World! ';
   });
 };
