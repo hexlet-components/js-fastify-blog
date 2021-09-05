bf3297946ed747cff5c51eab6f88594e4f23a71e
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2021-09-05 13:27:37 +0300

    add tests

--- plugins/sequelize.js
+++ plugins/sequelize.js
@@ -12,10 +12,11 @@ export default fp(async (fastify) => {
   });
 
   // NOTE: sequelize-fastify внутри использует колбеки
-  // в результате сервер останавливается ранше чем соединение с БД
+  // https://github.com/hsynlms/sequelize-fastify/blob/c3041ddfee4811777674167841fd5a18c3648fed/src/index.js#L22-L25
+  // в результате сервер останавливается раньше чем соединение с БД
   // и в тестах выводится Jest did not exit one second after the test run has completed.
-  // Также sequelize-fastify не загружает models и не добавляет их к инстансу
-  // это приодится делать вручную с помощью генерируемого sequelize фала models/index.cjs
+  // Также sequelize-fastify не загружает models и не добавляет их к инстансу,
+  // это приходится делать вручную с помощью генерируемого sequelize фала models/index.cjs
   // Таким образом открывается ещё одно соединение к БД.
   // В итоге вручную подключить sequelize проще и работает всё корректно.
   // Ниже закоментировано подключение с помощью sequelize-fastify
