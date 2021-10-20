cb95fe99819451387f2a65042bffcd970a2492e3
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2021-10-20 13:17:41 +0300

    fix typo

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // @ts-check
 
 import addRoutes from './routes/index.js';
-import registerPluging from './plugins/index.js';
+import registerPlugins from './plugins/index.js';
 
 export default async (fastify, _opts) => {
   const mode = process.env.NODE_ENV ?? 'development';
   fastify.decorate('mode', mode);
 
-  registerPluging(fastify);
+  registerPlugins(fastify);
   addRoutes(fastify);
 };
