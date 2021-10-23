5355e33e14c8d60bc84ead93cf0e1a52679d1e59
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2021-10-23 12:22:21 +0300

    fix deploy

--- config/config.cjs
+++ config/config.cjs
@@ -5,23 +5,19 @@ module.exports = {
     dialect: 'sqlite',
     storage: './database.sqlite',
   },
+  production: {
+    dialect: 'postgres',
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    host: process.env.DATABASE_HOST,
+  },
   test: {
     dialect: 'sqlite',
     storage: './database.test.sqlite',
   },
-  production: {
-    dialect: 'sqlite',
-    storage: './database.production.sqlite',
-  },
   // test: {
-  //   dialect: 'postgres',
-  //   database: process.env.DATABASE_NAME,
-  //   username: process.env.DATABASE_USERNAME,
-  //   password: process.env.DATABASE_PASSWORD,
-  //   port: process.env.DATABASE_PORT,
-  //   host: process.env.DATABASE_HOST,
-  // },
-  // production: {
   //   dialect: 'postgres',
   //   database: process.env.DATABASE_NAME,
   //   username: process.env.DATABASE_USERNAME,
