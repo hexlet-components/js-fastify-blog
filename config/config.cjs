94d10825b2ac43d6025ece7138f4fdc8062438e7
Nikolay Gagarinov <feycot@gmail.com>
2024-04-23 23:58:24 +0500

    fix config for tests

--- config/config.cjs
+++ config/config.cjs
@@ -13,16 +13,16 @@ module.exports = {
     port: process.env.DATABASE_PORT,
     host: process.env.DATABASE_HOST,
   },
-  // test: {
-  //   dialect: 'sqlite',
-  //   storage: './database.test.sqlite',
-  // },
   test: {
-    dialect: 'postgres',
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
-    host: process.env.DATABASE_HOST,
+    dialect: 'sqlite',
+    storage: './database.test.sqlite',
   },
+  // test: {
+  //   dialect: 'postgres',
+  //   database: process.env.DATABASE_NAME,
+  //   username: process.env.DATABASE_USERNAME,
+  //   password: process.env.DATABASE_PASSWORD,
+  //   port: process.env.DATABASE_PORT,
+  //   host: process.env.DATABASE_HOST,
+  // },
 };
